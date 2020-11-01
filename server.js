d9f8236fc62ae0dd357381022b174a747800ea29
Guilherme Dadald <guigasparotto@gmail.com>
2020-11-01 12:19:30 +0000

    Removed unnecessary GET request from server.js

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ const app = express();
 app.use(express.json({ extended: false }));
 
 // Tries to retrieve port from environment config, if unavailable uses default
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// Test simple get endpoint to the index page
-app.get("/", (req, res) => res.json({ msg: "Welcome to the Jungle" }));
 
 // Define other routes
 app.use("/api/users", require("./routes/users"));
